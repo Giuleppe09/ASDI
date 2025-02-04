ee212220a4a717081ce6afa68ca3996c67e27fc0
Giuseppe Castaldo <giuseppe.castaldo22@studenti.unina.it>
2025-02-04 18:02:31 +0100

    www

--- 2_RetiSequenzialiElementari/Cronometro_onDisplay/Cronometro_onDisplay/Cronometro_onDisplay.srcs/sources_1/imports/new/ButtonDebouncer.vhd
+++ 2_RetiSequenzialiElementari/Cronometro_onDisplay/Cronometro_onDisplay/Cronometro_onDisplay.srcs/sources_1/imports/new/ButtonDebouncer.vhd
@@ -1,29 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 28.10.2021 10:04:57
--- Design Name: 
--- Module Name: ButtonDebouncer - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
